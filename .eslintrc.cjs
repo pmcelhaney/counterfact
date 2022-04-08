cf2af50c4e15cf41f8214923ecccc5016f34a86a
Patrick McElhaney <pmcelhaney@gmail.com>
2022-04-08 09:37:29 -0400

    set up Jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = {
   extends: ["hardcore"],
 
@@ -5,4 +7,34 @@ module.exports = {
     node: true,
     es2022: true,
   },
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+
+  overrides: [
+    {
+      files: ["*.cjs"],
+      extends: ["hardcore"],
+
+      rules: {
+        "import/no-commonjs": "off",
+      },
+
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+
+    {
+      files: ["*.test.js"],
+      extends: ["hardcore/jest"],
+
+      rules: {
+        "import/unambiguous": "off",
+        "jest/prefer-expect-assertions": "off",
+      },
+    },
+  ],
 };
---
+++ test/hello.test.js
@@ -0,0 +1,5 @@
+describe("setting up Jest", () => {
+  it("runs", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
