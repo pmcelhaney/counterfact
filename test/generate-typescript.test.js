a375835dfca07c2189dc8c91bba41e4bfa768628
Patrick McElhaney <pmcelhaney@gmail.com>
2022-06-30 15:54:17 -0400

    remove the old generatePaths() code

--- test/generate-typescript.test.js
+++ test/generate-typescript.test.js
@@ -1,7 +1,4 @@
-import {
-  generatePaths,
-  generateTypeScript,
-} from "../src/generate-typescript.js";
+import { generateTypeScript } from "../src/generate-typescript.js";
 
 import { withTemporaryFiles } from "./lib/with-temporary-files.js";
 
@@ -99,49 +96,5 @@ describe("typescript generator", () => {
          export type HTTP_GET = ({ path } : { path: { name: string } }) => { body: string };
    `);
     });
-  });
-
-  it("generates a file for a path", () => {
-    const paths = {
-      "/pets": {
-        get: {
-          responses: {
-            default: {
-              content: {
-                "application/json": {
-                  schema: { $ref: "#/components/schemas/Pets" },
-                },
-              },
-            },
-          },
-        },
-      },
-    };
-
-    const files = generatePaths(paths);
-
-    expect(files).toStrictEqual([
-      {
-        name: "/pets.ts",
-
-        content: [
-          "import type { HTTP_GET } from './pets.types.ts';",
-          "",
-          "export const GET: HTTP_GET = () => {};",
-          "",
-        ].join("\n"),
-      },
-
-      {
-        name: "/pets.types.ts",
-
-        content: [
-          "import type { Pets } from ../schemas/Pets.ts;",
-          "",
-          "export type HTTP_GET = () => { body: Pets; };",
-          "",
-        ].join("\n"),
-      },
-    ]);
   });
 });
