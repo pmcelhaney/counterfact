0575d01305f36f1ece7c1b7b5ee1117ec7650825
Patrick McElhaney <pmcelhaney@gmail.com>
2022-07-13 17:26:29 -0400

    update todo list

--- src/tools.d.ts
+++
@@ -1,8 +0,0 @@
-import { JSONSchema6 } from "json-schema";
-
-export type Tools = {
-    oneOf: (array: string[]) => string;
-    randomFromSchema: (schema: JSONSchema6 ) => unknown;
-    accepts: (contentType: string) => boolean;
-}
- 
