073e018e1c5d41b1b467ff89e386346ff1dcbcd6
Patrick McElhaney <pmcelhaney@gmail.com>
2022-07-20 13:02:19 -0400

    characterization tests for ParametersTypeCoder

---
+++ test/typescript-generator/parameters-type-coder.test.js
@@ -0,0 +1,70 @@
+/* eslint-disable jest/no-restricted-matchers */
+import prettier from "prettier";
+
+import { ParametersTypeCoder } from "../../src/typescript-generator/parameters-type-coder.js";
+import { Requirement } from "../../src/typescript-generator/requirement.js";
+
+function format(code) {
+  return prettier.format(code, { parser: "typescript" });
+}
+
+describe("a ParametersTypeCoder", () => {
+  it("generates a list of potential names", () => {
+    const coder = new ParametersTypeCoder(new Requirement({}));
+
+    const [one, two, three] = coder.names();
+
+    expect([one, two, three]).toStrictEqual([
+      "parameters",
+      "parameters2",
+      "parameters3",
+    ]);
+  });
+
+  it("generates types for parameters", () => {
+    const requirement = new Requirement([
+      { name: "id", in: "path", schema: { type: "string" }, required: true },
+      { name: "name", in: "query", schema: { type: "string" } },
+      { name: "age", in: "query", schema: { type: "number" } },
+      { name: "name", in: "header", schema: { type: "string" } },
+    ]);
+
+    const pathCoder = new ParametersTypeCoder(requirement, "path");
+    const queryCoder = new ParametersTypeCoder(requirement, "query");
+    const headerCoder = new ParametersTypeCoder(requirement, "header");
+
+    expect(format(`type TestType =${pathCoder.write({})}`)).toMatchSnapshot(
+      "path"
+    );
+    expect(format(`type TestType =${queryCoder.write({})}`)).toMatchSnapshot(
+      "query"
+    );
+    expect(format(`type TestType =${headerCoder.write({})}`)).toMatchSnapshot(
+      "header"
+    );
+  });
+
+  it("generates type never when there is no match", () => {
+    const requirement = new Requirement([
+      { name: "id", in: "path", schema: { type: "string" }, required: true },
+    ]);
+
+    const coder = new ParametersTypeCoder(requirement, "query");
+
+    expect(format(`type TestType =${coder.write({})}`)).toMatchSnapshot();
+  });
+
+  it("has no type", () => {
+    const coder = new ParametersTypeCoder(new Requirement({}));
+
+    expect(coder.typeDeclaration(undefined, {})).toBe("");
+  });
+
+  it("calculates the modulePath", () => {
+    const coder = new ParametersTypeCoder(
+      new Requirement({}, "/components/parameters/foo")
+    );
+
+    expect(coder.modulePath()).toBe("parameters/parameters.types.ts");
+  });
+});
