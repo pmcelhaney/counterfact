88b6a11a1606a3ac97582c09211383ab6025fc0a
Patrick McElhaney <pmcelhaney@gmail.com>
2022-08-03 11:05:30 -0400

    counterfact start commmand
    currently this only works with JS files (not TS)
    
    updated the demo directory to reflect API changes

---
+++ src/start.js
@@ -0,0 +1,27 @@
+import nodePath from "node:path";
+
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+
+import { counterfact } from "./counterfact.js";
+
+const DEFAULT_PORT = 3100;
+
+export async function start(basePath = process.cwd(), port = DEFAULT_PORT) {
+  // eslint-disable-next-line import/no-unresolved, node/no-unsupported-features/es-syntax
+  const context = await import("./context/context.js").catch(() => ({}));
+
+  const app = new Koa();
+
+  app.use(bodyParser());
+
+  const { koaMiddleware } = await counterfact(
+    nodePath.join(basePath, "paths"),
+    context
+  );
+
+  app.use(koaMiddleware);
+
+  app.listen(port);
+  process.stdout.write(`Counterfact is running on port ${port}.\n`);
+}
