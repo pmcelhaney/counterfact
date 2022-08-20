c30bf1e4a98b43d87c566299de06936c7c64645d
Patrick McElhaney <pmcelhaney@gmail.com>
2022-08-20 19:57:52 -0400

    I think I know what I'm doing now but want to redo it properly with TDD

--- src/typescript-generator/response-type.coder.js
+++ src/typescript-generator/response-type.coder.js
@@ -2,7 +2,25 @@ import { Coder } from "./coder.js";
 import { SchemaTypeCoder } from "./schema-type-coder.js";
 
 export class ResponseTypeCoder extends Coder {
-  buildContentObjectType(script, responseCode, response) {
+  normalizeStatusCode(statusCode, responses) {
+    const definedStatusCodes = Object.keys(responses).filter(
+      (key) => key !== "default"
+    );
+
+    if (statusCode === "default") {
+      if (definedStatusCodes.length === 0) {
+        return "[statusCode in HttpStatusCode]";
+      }
+
+      return `[statusCode in Exclude<HttpStatusCode, ${definedStatusCodes.join(
+        " | "
+      )}>]`;
+    }
+
+    return statusCode;
+  }
+
+  buildContentObjectType(script, statusCode, response) {
     return `{
       ${Object.keys(response.content ?? {})
         .map(
@@ -10,7 +28,7 @@ export class ResponseTypeCoder extends Coder {
             `"${mediaType}": { 
               schema:  ${new SchemaTypeCoder(
                 this.requirement
-                  .select(responseCode)
+                  .select(statusCode)
                   .select("content")
                   .select(mediaType.replace("/", "~1"))
                   .select("schema")
@@ -27,7 +45,10 @@ export class ResponseTypeCoder extends Coder {
     return `{
       ${Object.entries(responses)
         .map(
-          ([responseCode, response]) => `${responseCode}: {
+          ([responseCode, response]) => `${this.normalizeStatusCode(
+            responseCode,
+            responses
+          )}: {
           headers: {};
           content: ${this.buildContentObjectType(
             script,
@@ -42,6 +63,7 @@ export class ResponseTypeCoder extends Coder {
 
   write(script) {
     script.importExternalType("ResponseBuilderBuilder", "counterfact");
+    script.importExternalType("HttpStatusCode", "counterfact");
 
     return `ResponseBuilderBuilder<${this.buildResponseObjectType(script)}>`;
   }
