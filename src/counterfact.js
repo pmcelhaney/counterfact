80a6b4c0abd82dc229f7e08f680f6e431fe5c5dc
Patrick McElhaney <pmcelhaney@gmail.com>
2022-09-09 15:48:28 -0400

    just trying to get CI to run

--- src/counterfact.js
+++ src/counterfact.js
@@ -1,6 +1,5 @@
 import fs from "node:fs/promises";
 import nodePath from "node:path";
-import os from "node:os";
 
 import yaml from "js-yaml";
 
@@ -8,7 +7,6 @@ import { Registry } from "./registry.js";
 import { Dispatcher } from "./dispatcher.js";
 import { koaMiddleware } from "./koa-middleware.js";
 import { ModuleLoader } from "./module-loader.js";
-import { Transpiler } from "./transpiler.js";
 
 async function loadOpenApiDocument(source) {
   try {
@@ -18,7 +16,6 @@ async function loadOpenApiDocument(source) {
   }
 }
 
-// eslint-disable-next-line max-statements
 export async function counterfact(
   basePath,
   context = {},
@@ -28,32 +25,13 @@ export async function counterfact(
 
   const registry = new Registry(context);
 
-  const modulesPath = `${await fs.mkdtemp(
-    nodePath.join(os.tmpdir(), "counterfact-")
-  )}/`;
-
-  try {
-    await fs.writeFile(
-      nodePath.join(modulesPath, "package.json"),
-      '{"type": "module"}'
-    );
-  } catch {
-    throw new Error("could not write package.json");
-  }
-
-  const transpiler = new Transpiler(basePath, modulesPath);
-
-  await transpiler.watch();
-
   const dispatcher = new Dispatcher(registry, openApiDocument);
-  const moduleLoader = new ModuleLoader(
-    nodePath.join(modulesPath, "paths"),
-    registry
-  );
+  const moduleLoader = new ModuleLoader(basePath, registry);
 
   await moduleLoader.load();
-
   await moduleLoader.watch();
 
   return { koaMiddleware: koaMiddleware(dispatcher), registry, moduleLoader };
 }
+
+// adding this comment to make CI kick off -- delete me
