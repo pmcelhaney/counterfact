87ab6eaea9bc9da71d982e3f294e7b8620abde0b
Patrick McElhaney <pmcelhaney@gmail.com>
2022-09-25 16:43:24 -0400

    remove the outdated demos

--- demo-ts/paths/store/inventory.ts
+++
@@ -1,4 +0,0 @@
-import type { HTTP_GET } from "./../../path-types/store/inventory.types.js";
-export const GET: HTTP_GET = ({ response }) => { 
-  return response[200].json({"ok": 1})
-};
--- demo/paths/hello/{name}.js
+++
@@ -1,7 +0,0 @@
-export function GET({ path, context, query, response }) {
-  context.visits ??= {};
-  context.visits[path.name] ??= 0;
-  context.visits[path.name] += 1;
-
-  return response["200"].text(`${query.greeting ?? "Hello"}, ${path.name}!`);
-}
