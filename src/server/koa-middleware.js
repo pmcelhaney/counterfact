3c8ae1d0607f95aa0213fe889e5de21fdd5c44a3
David Ethell <dethell@jhacorp.com>
2023-08-21 13:09:02 -0400

    fix(server): Fix lint errors

--- src/server/koa-middleware.js
+++ src/server/koa-middleware.js
@@ -2,6 +2,21 @@ import koaProxy from "koa-proxy";
 
 const HTTP_STATUS_CODE_OK = 200;
 
+function addCors(ctx, headers) {
+  // Always append CORS headers, reflecting back the headers requested if any
+  ctx.set("Access-Control-Allow-Origin", headers?.origin || "*");
+  ctx.set("Access-Control-Allow-Methods", "GET,HEAD,PUT,POST,DELETE,PATCH");
+  ctx.set(
+    "Access-Control-Allow-Headers",
+    headers?.["access-control-request-headers"]
+  );
+  ctx.set(
+    "Access-Control-Expose-Headers",
+    headers?.["access-control-request-headers"]
+  );
+  ctx.set("Access-Control-Allow-Credentials", "true");
+}
+
 export function koaMiddleware(dispatcher, options = {}, proxy = koaProxy) {
   return async function middleware(ctx, next) {
     const { method, path, headers, body, query } = ctx.request;
@@ -10,39 +25,26 @@ export function koaMiddleware(dispatcher, options = {}, proxy = koaProxy) {
       return proxy({ host: options.proxyUrl })(ctx, next);
     }
 
-    // Always append CORS headers
-    ctx.set("Access-Control-Allow-Origin", ctx.request?.headers?.origin || "*");
-    ctx.set("Access-Control-Allow-Methods", "GET,HEAD,PUT,POST,DELETE,PATCH");
-    ctx.set(
-      "Access-Control-Allow-Headers",
-      ctx.request?.headers?.["access-control-request-headers"]
-    );
-    ctx.set(
-      "Access-Control-Expose-Headers",
-      ctx.request?.headers?.["access-control-request-headers"]
-    );
-    ctx.set("Access-Control-Allow-Credentials", "true");
+    addCors(ctx, headers);
 
     // If the method is OPTIONS then always return OK
     if (method === "OPTIONS") {
       ctx.status = HTTP_STATUS_CODE_OK;
+    } else {
+      const response = await dispatcher.request({
+        method,
+        path,
+        headers,
+        body,
+        query,
+        req: ctx.req,
+      });
 
-      return undefined;
+      /* eslint-disable require-atomic-updates */
+      ctx.body = response.body;
+      ctx.status = response.status ?? HTTP_STATUS_CODE_OK;
+      /* eslint-enable require-atomic-updates */
     }
-
-    const response = await dispatcher.request({
-      method,
-      path,
-      headers,
-      body,
-      query,
-      req: ctx.req,
-    });
-
-    /* eslint-disable require-atomic-updates */
-    ctx.body = response.body;
-    ctx.status = response.status ?? HTTP_STATUS_CODE_OK;
-    /* eslint-enable require-atomic-updates */
 
     return undefined;
   };
