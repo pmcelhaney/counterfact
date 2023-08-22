78e5dbb3e408668de27896da6c4eea325c33e153
Patrick McElhaney <pmcelhaney@gmail.com>
2023-08-22 13:58:48 -0400

    replaceAll not replace

--- src/server/module-loader.js
+++ src/server/module-loader.js
@@ -23,7 +23,7 @@ export class ModuleLoader extends EventTarget {
 
   constructor(basePath, registry, contextRegistry = new ContextRegistry()) {
     super();
-    this.basePath = basePath.replace("\\", "/", "all");
+    this.basePath = basePath.replaceAll("\\", "/", "all");
     this.registry = registry;
     this.contextRegistry = contextRegistry;
   }
