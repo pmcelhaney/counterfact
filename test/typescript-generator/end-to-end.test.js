d54e63bf4ad9541e088cfa96c72514ccd0378eec
Patrick McElhaney <pmcelhaney@gmail.com>
2023-08-22 11:14:06 -0400

    break down tests further -- I think coder tests and the server tests are fine

--- test/typescript-generator/end-to-end.test.js
+++ test/typescript-generator/end-to-end.test.js
@@ -3,8 +3,7 @@
 import { generate } from "../../src/typescript-generator/generate.js";
 import { Repository } from "../../src/typescript-generator/repository.js";
 
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip("end-to-end test", () => {
+describe("end-to-end test", () => {
   it("generates the same code for pet store that it did on the last test run", async () => {
     const repository = new Repository();
 
