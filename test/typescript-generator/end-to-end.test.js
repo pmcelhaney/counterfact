576e4dc1662ea2ae675698de90b4e15eb0836275
Patrick McElhaney <pmcelhaney@gmail.com>
2023-08-22 14:52:00 -0400

    delete obsolete snapshot

--- test/typescript-generator/end-to-end.test.js
+++ test/typescript-generator/end-to-end.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-disabled-tests */
 /* eslint-disable jest/no-restricted-matchers */
 
 import { generate } from "../../src/typescript-generator/generate.js";
@@ -5,7 +6,7 @@ import { Repository } from "../../src/typescript-generator/repository.js";
 
 describe("end-to-end test", () => {
   // Skip because it fails on Windows and I can't figure out why
-  // eslint-disable-next-line jest/no-disabled-tests
+
   it.skip("generates the same code for pet store that it did on the last test run", async () => {
     const repository = new Repository();
 
