cc1de80d3e055fedf7f8c642e2b6dc6200affc6d
Patrick McElhaney <pmcelhaney@gmail.com>
2023-08-22 11:59:26 -0400

    add some logs to read-file.js

--- src/util/read-file.js
+++ src/util/read-file.js
@@ -2,16 +2,28 @@ import fs from "node:fs/promises";
 
 import nodeFetch from "node-fetch";
 
+function log(...strings) {
+  process.stdout.write(`[read-file] ${strings.join("\t")}\n`);
+}
+
 export async function readFile(urlOrPath) {
+  log("reading file or URL", urlOrPath);
+
   if (urlOrPath.startsWith("http")) {
+    log("reading url", urlOrPath);
+
     const response = await nodeFetch(urlOrPath);
 
     return response.text();
   }
 
   if (urlOrPath.startsWith("file")) {
+    log("reading file URl", urlOrPath);
+
     return fs.readFile(new URL(urlOrPath), "utf8");
   }
+
+  log("reading plain old file", urlOrPath);
 
   return fs.readFile(urlOrPath, "utf8");
 }
