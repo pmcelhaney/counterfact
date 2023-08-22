2cdb3875c8d6800bdcf6ea4b91486d4617afe74e
Patrick McElhaney <pmcelhaney@gmail.com>
2023-08-21 20:47:32 -0400

    come on Jest what is the deal

--- test/typescript-generator/specification.test.js
+++ test/typescript-generator/specification.test.js
@@ -61,6 +61,12 @@ describe("a Specification", () => {
     await new Promise((resolve) => {
       server.close(resolve);
     });
+
+    // The server still might not be completely shut down at this point so wait a bit longer
+    // eslint-disable-next-line promise/avoid-new
+    await new Promise((resolve) => {
+      setTimeout(resolve, 100);
+    });
   });
 
   it("returns a requirement for a URL", async () => {
