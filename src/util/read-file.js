6c61bac0857515f01eccef82ac3558387796bd1f
Patrick McElhaney <pmcelhaney@gmail.com>
2023-08-22 13:15:30 -0400

    maybe readFileSync will work?

--- src/util/read-file.js
+++ src/util/read-file.js
@@ -1,5 +1,6 @@
-/* eslint-disable no-magic-numbers */
-import fs from "node:fs/promises";
+/* eslint-disable n/no-sync */
+
+import fs from "node:fs";
 
 import nodeFetch from "node-fetch";
 
@@ -22,7 +23,7 @@ export async function readFile(urlOrPath) {
   if (urlOrPath.startsWith("file")) {
     log("reading file URl", urlOrPath);
 
-    return fs.readFile(new URL(urlOrPath), "utf8");
+    return fs.readFileSync(new URL(urlOrPath), "utf8");
   }
 
   log("reading plain old file", urlOrPath);
@@ -31,7 +32,7 @@ export async function readFile(urlOrPath) {
   let result;
 
   try {
-    result = fs.readFile(urlOrPath, "utf8");
+    result = fs.readFileSync(urlOrPath, "utf8");
 
     log("did read plain old file", urlOrPath);
   } catch (error) {
@@ -39,17 +40,6 @@ export async function readFile(urlOrPath) {
 
     return `ERROR: ${error}`;
   }
-
-  log("waiting for result to resolve");
-
-  // eslint-disable-next-line promise/avoid-new
-  const timeout = new Promise((resolve) => {
-    setTimeout(() => resolve("COULD NOT READ FILE IN A REASONABLE TIME"), 1000);
-  });
-
-  const text = await Promise.race([timeout, result]);
-
-  log("returning result", text);
 
   return result;
 }
--- test/just-read-the-petstore-file.test.js
+++ test/just-read-the-petstore-file.test.js
@@ -11,6 +11,14 @@ test("reads the petstore file using readFile", async () => {
   expect(text).toStrictEqual(expect.stringContaining("openapi: 3.0.2"));
 });
 
+test("reads the petstore file using readFile (again)", async () => {
+  const text = await readFile("./petstore.yaml", "utf8").catch(
+    (error) => error
+  );
+
+  expect(text).toStrictEqual(expect.stringContaining("openapi: 3.0.2"));
+});
+
 test("reads the petstore file", async () => {
   const text = await fs
     .readFile("./petstore.yaml", "utf8")
