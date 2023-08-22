1c3db94c6f6c1403fc0e179c543e506f1e077509
Patrick McElhaney <pmcelhaney@gmail.com>
2023-08-22 11:22:45 -0400

    The problem appears to be in specification.js. Adding some more logs.

--- src/typescript-generator/specification.js
+++ src/typescript-generator/specification.js
@@ -6,25 +6,45 @@ import { readFile } from "../util/read-file.js";
 
 import { Requirement } from "./requirement.js";
 
+function log(...strings) {
+  process.stdout.write(`[specification] ${strings.join("\t")}\n`);
+}
+
 export class Specification {
   constructor(rootUrl) {
     this.cache = new Map();
     this.rootUrl = rootUrl;
   }
 
+  // eslint-disable-next-line max-statements
   async requirementAt(url, fromUrl = "") {
+    log("requirementAt", url, fromUrl);
+
     const [file, path] = url.split("#");
 
     const filePath = nodePath
       .join(fromUrl.split("#").at(0), file)
       .replaceAll("\\", "/");
 
+    log("filePath", filePath);
+
     const fileUrl = filePath === "." ? this.rootUrl : filePath;
+
+    log("about to load fileUrl", fileUrl);
+
     const data = await this.loadFile(fileUrl);
+
+    log("finished loading fileUrl", fileUrl);
 
     const rootRequirement = new Requirement(data, `${fileUrl}#`, this);
 
-    return rootRequirement.select(path.slice(1));
+    log("looking up requirement at path", path);
+
+    const result = rootRequirement.select(path.slice(1));
+
+    log("got requirement", JSON.stringify(result));
+
+    return result;
   }
 
   async loadFile(urlOrPath) {
