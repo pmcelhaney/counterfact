8ab35b9f4082a06c17950407da9a7cc36bebd083
Patrick McElhaney <pmcelhaney@gmail.com>
2024-01-16 13:04:40 -0500

    generate the right type of module (ESM or CJS)

--- test/server/deterimine-module-kind.test.ts
+++ test/server/deterimine-module-kind.test.ts
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+// To do: add types to usingTemporaryFiles package
+
 import { usingTemporaryFiles } from "using-temporary-files";
 
-import { getModuleKind } from "../../src/typescript-generator/get-module-kind.js";
+import { determineModuleKind } from "../../src/server/determine-module-kind.js";
 
 describe("getModuleKind", () => {
   it("returns the module type from package.json in the current directory", async () => {
     await usingTemporaryFiles(async ({ add, path }) => {
       await add("package.json", JSON.stringify({ type: "module" }));
 
-      await expect(getModuleKind(path("."))).resolves.toBe("module");
+      await expect(determineModuleKind(path("."))).resolves.toBe("module");
     });
   });
 
@@ -18,9 +22,19 @@ describe("getModuleKind", () => {
         JSON.stringify({ type: "module" }),
       );
 
-      await expect(getModuleKind(path("./subdirectory"))).resolves.toBe(
+      await expect(determineModuleKind(path("./subdirectory"))).resolves.toBe(
         "module",
       );
+    });
+  });
+
+  it("returns the module type from package.json in a grandparent directory", async () => {
+    await usingTemporaryFiles(async ({ add, path }) => {
+      await add("deep/down/package.json", JSON.stringify({ type: "module" }));
+
+      await expect(
+        determineModuleKind(path("./deep/down/even/further/down")),
+      ).resolves.toBe("module");
     });
   });
 
@@ -28,13 +42,13 @@ describe("getModuleKind", () => {
     await usingTemporaryFiles(async ({ add, path }) => {
       await add("package.json", JSON.stringify({}));
 
-      await expect(getModuleKind(path("."))).resolves.toBe("commonjs");
+      await expect(determineModuleKind(path("."))).resolves.toBe("commonjs");
     });
   });
 
   it("returns commonjs if no package.json is found", async () => {
     await usingTemporaryFiles(async ({ path }) => {
-      await expect(getModuleKind(path("."))).resolves.toBe("commonjs");
+      await expect(determineModuleKind(path("."))).resolves.toBe("commonjs");
     });
   });
 });
