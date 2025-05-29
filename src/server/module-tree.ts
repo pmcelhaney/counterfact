0cae6b13d3a5aa4a19da27495a82b1dbb4a994bf
David Ethell <dethell@jhacorp.com>
2025-05-29 13:51:02 -0400

    changes per review

--- src/server/module-tree.ts
+++ src/server/module-tree.ts
@@ -136,16 +136,17 @@ export class ModuleTree {
     matchedPath: string,
     method: string,
   ) {
-    let fileMatch = "";
-    for (const file in directory.files) {
-      if (file.toLowerCase() === segment.toLowerCase()) {
-        fileMatch = file;
-        break;
+    function normalizedSegment(segment: string, directory: Directory) {
+      for (const file in directory.files) {
+        if (file.toLowerCase() === segment.toLowerCase()) {
+          return file;
+        }
       }
+      return "";
     }
 
     const match =
-      directory.files[fileMatch] ??
+      directory.files[normalizedSegment(segment, directory)] ??
       Object.values(directory.files).find(
         (file) => file.isWildcard && this.fileModuleDefined(file, method),
       );
