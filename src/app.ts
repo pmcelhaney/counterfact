80e2170fee0e4d84a427f4d5acc7c917b29cb34f
David Ethell <dethell@jhacorp.com>
2025-06-20 10:29:54 -0400

    wip

--- src/app.ts
+++ src/app.ts
@@ -145,7 +145,7 @@ export async function counterfact(config: Config) {
   const transpiler = new Transpiler(
     nodePath.join(modulesPath, "routes").replaceAll("\\", "/"),
     compiledPathsDirectory,
-    "commonjs",
+    "module",
   );
 
   const moduleLoader = new ModuleLoader(
