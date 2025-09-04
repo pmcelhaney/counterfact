eda9fc83fbe4f525c97253c07a400b2b94111b19
Patrick McElhaney <pmcelhaney@gmail.com>
2025-09-04 17:53:59 -0400

    I think I fixed it

--- src/util/windows-escape.js
+++ src/util/windows-escape.js
@@ -2,7 +2,11 @@ const UNICODE_RATIO_SYMBOL = "∶"; // U+2236
 const REGULAR_COLON = ":";
 
 export function escapePathForWindows(path) {
-  return path.replaceAll(REGULAR_COLON, UNICODE_RATIO_SYMBOL);
+  return path.replace(
+    /^([A-Za-z]):(.*)/,
+    (_, drive, rest) =>
+      `${drive}:${rest.replaceAll(REGULAR_COLON, UNICODE_RATIO_SYMBOL)}`,
+  );
 }
 
 export function unescapePathForWindows(path) {
