c627388f56405c359c7e7cb505b08be4f56f50d8
Jordan Running <jordan.running@banno.com>
2026-01-06 16:36:49 -0600

    Fix lint error

--- src/server/convert-js-extensions-to-cjs.ts
+++ src/server/convert-js-extensions-to-cjs.ts
@@ -2,6 +2,7 @@ export function convertFileExtensionsToCjs(code: string) {
   // Match require('...') or require("...") where the path starts with .
   // Replace .js or .ts extensions (or no extension) with .cjs
   return code.replace(
+    // eslint-disable-next-line regexp/prefer-named-capture-group
     /require\((['"])(\.[^'"]*?)(\.(?:js|ts))?\1\)/g,
     (match, quote, path) => `require(${quote}${path}.cjs${quote})`,
   );
